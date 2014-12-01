e8e8717b6daafcbcb6425a8b916b472edcdc0fc5
Lucas Amaral <lucasmouraamaral@gmail.com>
2014-12-01 17:06:41 -0200

    Removido pasta inutil de fora. Adicionado tempos.

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 .PHONY: prog
 
-CFLAGS=-ansi -g  -std=c++11 -Wall 
+CFLAGS=-ansi -g  -std=c++11 -Wall -O6
 
 all: prog
 
 prog: main.cpp 
-	g++ $(CFLAGS) main.cpp -o fptree.out -fopenmp
+	g++ $(CFLAGS) main.cpp -o fptree.out -lpthread
 
 clean:
 	rm -f *.o
--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#include <iterator>
 #include <list>
 #include <map>
 #include <cstdlib>
-#include "tree.hpp"
+#include <chrono>
+#include "fp_tree.hpp"
 #include "transaction.hpp"
+#include "parallel.hpp"
 
 using namespace std;
 
@@ -17,16 +20,31 @@ template<typename T>
 void print_tree(Tree_Node<T>* node);
 table_transaction_t<int> toMem(string filename);
 
-int main()
+int THREAD_COUNT = 1;
+
+int main(int argc, char** argv)
 {
-	//int supp=2;
-	int supp=3;
+	if (argc < 3) {
+		cout << "Uso: input_name supp [num_threads = 1]" << endl;
+		return 1;
+	} 
+	else if(argv[3])
+	{
+		THREAD_COUNT = atoi(argv[3]);
+		if(!THREAD_COUNT)
+		{
+			cout << "Uso: input_name supp [num_threads = 1]" << endl;
+			return 1;
+		}
+	}
+
+	int supp = atoi(argv[2]);
+	string filename(argv[1]);
 	cout << "Iniciando programa" << endl;
 	cout << "Passando para a memória" << endl;
 
-	//table_transaction_t<int> tr_table = toMem("T40I10D100K.db");
-	table_transaction_t<int> tr_table = toMem("items.db");
-	header_table<int> *table;
+
+	table_transaction_t<int> tr_table = toMem(filename.c_str());
 
 	cout << "Achando frequencias" << endl;
 	map<int, int> freqs =  find_frequency<int>(tr_table);
@@ -39,30 +57,55 @@ int main()
 		elements.push_back(pair.first);
 	}
 
-	cout << "Quantidade de elementos:" << elements.size() << endl;
-
 	sort(elements.begin(),elements.end(),freq_obj);
-	//elements.sort(freq_obj);
 
-	table = new header_table<int>();
-
-	cout << "Construindo FPTree" << endl;
-	Tree_Node<int> *root = Tree_Node<int>::build_fptree(tr_table, table, freqs, freq_obj, supp);
-	print_tree(root);
-
-	cout << "~~~~";
-
-	table->clear();
-	delete table;
-	cout << endl << "----";
-
-	cout << "Iniciando FPGrowth" << endl;
-	loop_fp<int>(supp,root, elements);
-
-	if (root != NULL) {
-		delete root;
+	if (elements.size() < (unsigned) THREAD_COUNT) {
+		cout << endl << "Quantidade de elementos fornecidos é menor que a quantidade de threads. Quantidade de threads será limitada." << endl << endl;
+		THREAD_COUNT = elements.size();
 	}
+
+	auto t3 = std::chrono::high_resolution_clock::now();
+	cout << "Construindo FPtree[";
+	auto sub_trees = build_tree_parallel<int, table_transaction_t<int>::iterator>(tr_table, freqs, supp );
+	auto t4 = std::chrono::high_resolution_clock::now();
+	cout << chrono::duration_cast<chrono::microseconds>(t4 - t3).count() << "]" << endl;
+
+	auto t1 = std::chrono::high_resolution_clock::now();
+	cout << "Iniciando FPGrowth[" ;
+	auto all_extracts = parallel_loop_fp<int>(supp,sub_trees[0], elements);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	cout << chrono::duration_cast<chrono::microseconds>(t2 - t1).count() << "]" << endl;
+
+	fstream file;
+	file.open("output.txt", std::fstream::trunc | std::fstream::out);
+
+	for(auto it = all_extracts->begin(); it != all_extracts->end(); it++) {
+		print_frequent_list(file, *it);
+		file << endl;
+	}
+
+	cout << "Finalizando FPGrowth" << endl;
+
+	auto it1 = all_extracts->begin();
+	while(it1 != all_extracts->end()) {
+		auto it2 = (*it1)->begin();
+		while(it2 != (*it1)->end()) {
+			auto it3 = (*it2)->begin();
+			while(it3 != (*it2)->end()) {
+				it3 = (*it2)->erase(it3);
+			}
+			it2 = (*it1)->erase(it2);
+		}
+		delete *it1;
+		it1 = all_extracts->erase(it1);
+	}
+
+	delete all_extracts;
+
+	delete sub_trees[0];
 	
+	delete [] sub_trees;
+
 	return 0;
 }
 template<typename T>
@@ -82,7 +125,6 @@ table_transaction_t<int> toMem(string filename)
 	sc.open(filename.c_str());
 	while(getline(sc,s))
 	{
-		//cout << ".";
 		if(s!="")
 		{
 			int aux;
